664baf055878b393a50852610662a94192a91661
Víctor Castrillo M <victor.castrillo.99@gmail.com>
2019-10-26 15:47:34 -0700

    [update] Login Activity

--- app/src/main/java/com/stardust/programathon2019/LoginActivity.java
+++ app/src/main/java/com/stardust/programathon2019/LoginActivity.java
@@ -3,12 +3,28 @@ package com.stardust.programathon2019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText id_entry;
+    private EditText password_entry;
+    private Button login_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        id_entry = findViewById(R.id.id_entry);
+        password_entry = findViewById(R.id.password_entry);
+        login_button = findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Code for data validation
+            }
+        });
     }
 }
