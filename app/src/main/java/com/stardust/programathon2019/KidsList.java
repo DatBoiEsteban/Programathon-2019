774b3be71a2868d9e6a6bb95616f3aff7b529a73
Esteban Mata <esteban1113001@gmail.com>
2019-10-27 05:39:55 -0600

    datepicker

--- app/src/main/java/com/stardust/programathon2019/KidsList.java
+++ app/src/main/java/com/stardust/programathon2019/KidsList.java
@@ -1,5 +1,7 @@
 package com.stardust.programathon2019;
 
+import android.app.DatePickerDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.app.Dialog;
 import android.graphics.Color;
@@ -17,21 +19,35 @@ import com.stardust.programathon2019.Controller.SessionManager;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.InputType;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 public class KidsList extends AppCompatActivity implements AwaitableResponse {
     private KidsList instance = this;
     private TableLayout table;
     private ImageButton backButton;
+    private Kid currentKid;
+    private int[] date = {0,0,0};
+
+    public Kid getCurrentKid() {
+        return currentKid;
+    }
+
+    public void setCurrentKid(Kid currentKid) {
+        this.currentKid = currentKid;
+    }
 
     private Dialog dialog;
     @Override
@@ -74,13 +90,39 @@ public class KidsList extends AppCompatActivity implements AwaitableResponse {
 
     public void searchKid(View v) {
         dialog.setContentView(R.layout.query_by_kid);
-        TextInputLayout studentName = dialog.findViewById(R.id.query_by_name_consult_text);
+        final TextInputLayout studentName = dialog.findViewById(R.id.query_by_name_consult_text);
         Button btnConsult = dialog.findViewById(R.id.query_by_name_consult_button);
+        final EditText eText = dialog.findViewById(R.id.query_by_name_date_text);
+        eText.setInputType(InputType.TYPE_NULL);
+        final Context ctx = v.getContext();
+        eText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                DatePickerDialog picker = new DatePickerDialog(ctx,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                eText.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                                date[0] = dayOfMonth;
+                                date[1] = monthOfYear + 1;
+                                date[2] = year;
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+        final SearchListener searchListener = new SearchListener(this);
         btnConsult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dialog.dismiss();
-                showKid(getCurrentFocus());
+                StudentController.getStudentByDNI(Integer.parseInt(studentName.getEditText().getText().toString()),searchListener );
+
             }
         });
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -90,6 +132,13 @@ public class KidsList extends AppCompatActivity implements AwaitableResponse {
     public void showKid(View v) {
         dialog.setContentView(R.layout.query_by_kid_info);
         dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        TextView dni = dialog.findViewById(R.id.query_by_name_dni_info);
+        TextView dateText = dialog.findViewById(R.id.query_by_name_date_info);
+        dateText.setText(date[0] + "/" + date[1] + "/" + date[2]);
+        TextView asq = dialog.findViewById(R.id.query_by_name_asq3_info);
+        dni.setText(currentKid.getFirstName() + " " + currentKid.getLastName());
+        //asq.setText(currentKid.getForm().getName());
+
         dialog.show();
 
     }
@@ -121,6 +170,12 @@ public class KidsList extends AppCompatActivity implements AwaitableResponse {
         }
     }
 
+    public void showKidResult(Kid kid){
+        showKid(getCurrentFocus());
+
+    }
+
+
     @Override
     public void onComplete(Object obj) {
         if (obj == null) {
@@ -129,5 +184,23 @@ public class KidsList extends AppCompatActivity implements AwaitableResponse {
         Kid[] kids = (Kid[])obj;
 
         updateTable(kids);
+    }
+    private class SearchListener implements AwaitableResponse {
+        KidsList kidsList;
+
+        SearchListener(KidsList kidsList){
+            this.kidsList = kidsList;
+        }
+
+        @Override
+        public void onComplete(Object obj) {
+            if (obj == null) {
+                return;
+            }
+            kidsList.setCurrentKid((Kid)obj);
+            kidsList.showKidResult(((Kid)obj) );
+        }
+
+
     }
 }
