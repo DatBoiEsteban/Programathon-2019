eaac9788d9caae0272d6b841ce51576ee726080d
Esteban Mata <esteban1113001@gmail.com>
2019-10-27 01:22:33 -0600

    internet validation

--- app/src/main/java/com/stardust/programathon2019/LoginActivity.java
+++ app/src/main/java/com/stardust/programathon2019/LoginActivity.java
@@ -2,7 +2,9 @@ package com.stardust.programathon2019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -79,7 +81,7 @@ public class LoginActivity extends AppCompatActivity implements Awaitable {
     }
 
     public void confirmInput(View v) {
-        if (isInternetAvailable()) {
+        if (isOnline()) {
             if (!validateID() | !validatePassword()) {
                 return;
             }
@@ -93,14 +95,9 @@ public class LoginActivity extends AppCompatActivity implements Awaitable {
 
     }
 
-    public boolean isInternetAvailable() {
-        try {
-            InetAddress ipAddr = InetAddress.getByName("google.com");
-            //You can replace it with your name
-            return !ipAddr.equals("");
-        } catch (Exception e) {
-            return false;
-        }
+    public boolean isOnline() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        return cm.getActiveNetworkInfo() != null && cm.getActiveNetworkInfo().isConnectedOrConnecting();
     }
 
     @Override
@@ -113,7 +110,6 @@ public class LoginActivity extends AppCompatActivity implements Awaitable {
         } else {
             Toast.makeText(this, "El usuario o contraseña son incorrectos", Toast.LENGTH_SHORT).show();
         }
-        StudentController.getStudentByName("Martin");
 
     }
 
