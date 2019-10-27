ee807e27f89ff39c35f4cfafbce123c3f7e60c1a
Víctor Castrillo M <victor.castrillo.99@gmail.com>
2019-10-26 20:18:10 -0700

    [add] Kids table

---
+++ app/src/main/java/com/stardust/programathon2019/KidsList.java
@@ -0,0 +1,37 @@
+package com.stardust.programathon2019;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+
+public class KidsList extends AppCompatActivity {
+    private TableLayout table;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kids_list);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        table = findViewById(R.id.kidstable);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
